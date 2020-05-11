b23cc41969e508dc751417666da3870ebeea339f
javaxiss <yissamo@gmail.com>
2020-05-11 18:26:45 +0200

    checkpoint : ready for 14

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             Edit <code>src/App.js</code> and save to reload.
           </p>
 
-          <h1>Fib Calculator</h1>
+          <h1>FIB CALCULATOR V2</h1>
           <Link to="/" >Home</Link>
           <Link to="/otherpage" >Other Page</Link>
         </header>
